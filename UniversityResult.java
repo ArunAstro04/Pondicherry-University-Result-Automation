b94c6b77155a87f55d7cc00dece32121043f2c77
Arunkumar Kabilan <arunastro1999@gmail.com>
2020-11-06 22:37:30 +0530

    Update UniversityResult.java

--- UniversityResult.java
+++ UniversityResult.java
@@ -19,8 +19,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UniversityResult {
 	public static void main(String a[]) throws FileNotFoundException, IOException{
-		
-		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		String semester = "Eight"
+		//WebDriver for Automation
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");	//Path of Driver and property name
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://exam.pondiuni.edu.in/oresults/");
@@ -41,43 +42,35 @@ public class UniversityResult {
 		
 		//Loop to execute automation for rowLastNo times
 		while(rows.hasNext()){
-			
 			XSSFCell rollNo=wb.getSheetAt(0).getRow(index).getCell(0);	//Getting rollNo from input excel sheet
-			
 			driver.findElement(By.id("reg_no")).sendKeys(rollNo.toString());  //Sending rollNo values to Register Number text box
-			driver.findElement(By.id("exam")).sendKeys("Sixth");	//Choosing semester from drop down list box
+			driver.findElement(By.id("exam")).sendKeys(semester);	//Choosing semester from drop down list box
 			driver.findElement(By.xpath("//*[@id=\"print_app_form\"]")).click();	//Clicking on submit button 
-			
 			WebDriverWait wait = new WebDriverWait(driver, 60);		//Driver wait
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/table[1]/tbody/tr[2]/td"))); 
-			
-			//For Row One Titles
+			//For Row One Headers
 			if(lineNo==0){
-				
 				head.add("Register Number");
 				head.add("Name");
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[2]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[3]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[4]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[5]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[6]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[7]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[8]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[9]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[10]/td[2]")).getText());
-	        	head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[11]/td[2]")).getText());
-	        	
-	        	Row  = sheet.createRow(lineNo);
-	        	for (int i = 0; i < head.size(); i++) 
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[2]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[3]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[4]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[5]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[6]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[7]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[8]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[9]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[10]/td[2]")).getText());
+	        		head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[11]/td[2]")).getText());
+	        		Row  = sheet.createRow(lineNo);
+	        		for (int i = 0; i < head.size(); i++) 
 				{	
-	        		cell = Row.createCell(i);
-	        		cell.setCellValue(head.get(i));
-	        		sheet.autoSizeColumn(i);
+	        			cell = Row.createCell(i);
+	        			cell.setCellValue(head.get(i));
+	        			sheet.autoSizeColumn(i);
 				}
-	        	
-	        	head.clear();
-	        	
-	        	lineNo++;
+	        		head.clear();
+	        		lineNo++;
 			}
 			head.add(rollNo.toString());
 			head.add(driver.findElement(By.xpath("//*[@id=\"student_info\"]/tbody/tr[3]/td")).getText().substring(21));
@@ -91,23 +84,20 @@ public class UniversityResult {
 			head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[9]/td[7]")).getText());
 			head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[10]/td[7]")).getText());
 			head.add(driver.findElement(By.xpath("//table[@id='results_subject_table']/tbody/tr[11]/td[7]")).getText());
-			
 			Row  = sheet.createRow(lineNo);
-			
 			for (int j=0; j < head.size(); j++) 
 			{	
-        		cell = Row.createCell(j);
-        		cell.setCellValue(head.get(j));
-        		sheet.autoSizeColumn(j);
-        		
-        		//For console output	
-        		System.out.print(head.get(j)+" ");
+				cell = Row.createCell(j);
+				cell.setCellValue(head.get(j));
+				sheet.autoSizeColumn(j);
+        			//For console output	
+        			System.out.print(head.get(j)+" ");
 			}
 			System.out.println();
 			head.clear();
 			lineNo++;
-        	index++;
-        	driver.navigate().back();
+        		index++;
+        		driver.navigate().back();
 			if(lineNo==(rowLastNo+1)){
 				workbook.write(fileOut);
 				break;
